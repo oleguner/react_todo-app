34e6da6fdea925e03eb9318cbcb50f7b05599806
Oleg Bezriadin <muhachov86@gmail.com>
2021-04-22 10:39:45 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.a9bfa3678305ca1290835fab7e7ea03b.js"
 );
 
 self.addEventListener('message', (event) => {
